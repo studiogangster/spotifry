8f615ed67f6c016d991c88d7e84e6052b4979121
Prakhar Agnihotri <rounak316@gmail.com>
2021-07-06 17:46:58 +0530

    nginx +lowkey

---
+++ lib/init.sh
@@ -0,0 +1,7 @@
+export PATH=$PATH:/Users/meddler/Workspaces/pentest/apkpatcher
+
+ANDROID_HOME=/Users/$USER/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/build-tools/30.0.3
+
+alias frida-compile='/Users/meddler/Workspaces/pentest/apkpatcher/node_modules/frida-compile/bin/compile.js'
+# '/node_modules/frida-compile/bin/compile.js'
